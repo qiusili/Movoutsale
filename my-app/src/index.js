0c7cc2c645f40710c0873fbac1e8e5b76d5ba5de
Qiusi Li <qiusili@usc.edu>
2019-03-17 15:45:30 -0700

    Add category bar

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './navbar';
 
-
 class App extends Component {
   render(){
     return(
--- my-app/src/navbar.js
+++ my-app/src/navbar.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Nav, Navbar, Button }  from 'react-bootstrap';
 
-export default class NavBar extends Component {
+import { Navbar, Nav, Button } from 'react-bootstrap';
+
+class NavBar extends Component {
+
+  state = {
+    catogries : [ 'Bedframe', 'Mattress', 'Desk', 'ArmChair', 'Sofa' ]
+  }
+
   render(){
-    console.log("HERE");
 
     return(
+      <React.Fragment>
+
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand href="#home">MovoutSale</Navbar.Brand>
-
           <Nav className="mr-auto">
-            <Nav.Link href="#Dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="#Dashboard" disabled>Dashboard</Nav.Link>
           </Nav>
           <Button variant="outline-info">Log in</Button>
         </Navbar>
 
-    );
+        <Nav className="justify-content-center" variant="tabs">
+          {
+            this.state.catogries.map((catogry,index) =>
+            <Nav.Item>
+              <Nav.Link eventKey={`link-${index}`}>
+                {catogry}
+              </Nav.Link>
+            </Nav.Item>)
+          }
+        </Nav>
+
+      </React.Fragment>
+    )
   }
 }
+
+export default NavBar;
